93b5ac975b7cf9373bd7c52b2173acae8423f56d
Nikos M <nikosms0@gmail.com>
2015-04-10 11:28:55 +0300

    beeld config updates

--- css/make-css.bat
+++ css/make-css.bat
@@ -1,5 +1,5 @@
 @echo off
 
-rem python %BUILDTOOLS%\Beeld.py --config ".\config.custom" --compiler cssmin
-rem php -f %BUILDTOOLS%\Beeld.php -- --config=".\config.custom" --compiler=cssmin
-node %BUILDTOOLS%\Beeld.js --config ".\config.custom" --compiler cssmin
+rem python %BUILDTOOLS%\Beeld.py --config ".\beeld.config" --compiler cssmin
+rem php -f %BUILDTOOLS%\Beeld.php -- --config=".\beeld.config" --compiler=cssmin
+node %BUILDTOOLS%\Beeld.js --config ".\beeld.config" --compiler cssmin
